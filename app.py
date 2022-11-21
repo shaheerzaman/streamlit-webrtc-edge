60c7ba14582610a6e97c1c8be99bfcc0523eb35c
Zaman, Shaheer Shaheer <zamans@deshaw.com>
2022-11-21 23:51:40 +0530

    second commit

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from streamlit_webrtc import webrtc_streamer
 import av
 import cv2
 
-st.title("My first streamlit app")
-st.write("Hello world!")
+st.title("My first Streamlit app")
+st.write("Hello, world")
 
 
 class VideoProcessor:
@@ -14,9 +14,11 @@ class VideoProcessor:
 
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
+
         img = cv2.cvtColor(
             cv2.Canny(img, self.threshold1, self.threshold2), cv2.COLOR_GRAY2BGR
         )
+
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
@@ -25,7 +27,6 @@ ctx = webrtc_streamer(
     video_processor_factory=VideoProcessor,
     rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
 )
-
 if ctx.video_processor:
     ctx.video_processor.threshold1 = st.slider(
         "Threshold1", min_value=0, max_value=1000, step=1, value=100
